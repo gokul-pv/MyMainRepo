322d2e3d30631fc365cdd5a41b98d5d5083ed427
Gokul <25975535+gokul-pv@users.noreply.github.com>
2021-07-02 20:59:12 +0530

    Update utils1.py

--- utils1.py
+++ utils1.py
@@ -40,3 +40,30 @@ def visualize_augmentations(dataset,transforms, idx=0, samples=10, cols=5 ):
       ax.ravel()[i].set_axis_off()
   plt.tight_layout()
   plt.show()
+
+  
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torchvision
+
+
+# functions to show an image
+def imshow(img,c ):
+#   img = img / 2 + 0.5     # unnormalize
+  npimg = img.numpy()
+  fig = plt.figure(figsize=(7,7))
+  plt.imshow(np.transpose(npimg, (1, 2, 0)),interpolation='none')
+  plt.title(c)
+
+
+def show_train_data(dataset, classes):
+
+	# get some random training images
+  dataiter = iter(dataset)
+  images, labels = dataiter.next()
+  for i in range(10):
+    index = [j for j in range(len(labels)) if labels[j] == i]
+    imshow(torchvision.utils.make_grid(images[index[0:5]],nrow=5,padding=2,scale_each=True),classes[i])  
+  
+  
