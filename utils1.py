f0545115d019ff607e5de2f5e9a32e0f38e11d15
Gokul <25975535+gokul-pv@users.noreply.github.com>
2021-07-02 19:36:06 +0530

    Update utils1.py

--- utils1.py
+++ utils1.py
@@ -2,7 +2,6 @@ from torchvision import transforms
 import albumentations as A
 import albumentations.pytorch as AP
 import numpy as np
-import random
 import copy
 import matplotlib.pyplot as plt
 
@@ -25,6 +24,8 @@ class AlbumentationTransforms:
   
   
 def visualize_augmentations(dataset,transforms, idx=0, samples=10, cols=5 ):
+  MEAN = torch.tensor([0.485, 0.456, 0.406])
+  STD = torch.tensor([0.229, 0.224, 0.225])
   dataset = copy.deepcopy(dataset)
   dataset.transform = transforms
   rows = samples // cols
