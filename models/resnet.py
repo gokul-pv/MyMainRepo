250ab971139e9b01bf93b7dfe0f5802f97020a06
Gokul <25975535+gokul-pv@users.noreply.github.com>
2021-07-23 19:52:35 +0530

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -36,7 +36,8 @@ class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
         self.in_planes = 64
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,stride=1, padding=13, bias=False)
+#         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,stride=1, padding=13, bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,stride=1, padding=1, bias=False)
         self.bn1   = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
@@ -58,16 +59,18 @@ class ResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = F.avg_pool2d(out, 7)
+#         out = F.avg_pool2d(out, 7)
+        out = F.avg_pool2d(out, 8)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
 
 
-def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
+def ResNet18(num_classes = 10):
+    return ResNet(BasicBlock, [2, 2, 2, 2], num_classes = num_classes)
 
-
+# def ResNet18():
+#     return ResNet(BasicBlock, [2, 2, 2, 2])
 # def ResNet34():
 #     return ResNet(BasicBlock, [3, 4, 6, 3])
 
